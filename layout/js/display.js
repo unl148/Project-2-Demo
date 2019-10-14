68438c06721c8e07dc9311f156a6ddfdf4e25581
konstagap <konstagapov123@gmail.com>
2019-10-14 11:52:02 -0700

    added javaScript files, edited html files

---
+++ layout/js/display.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $.get("/api/search/:" + userId + ":" + curentEdit).then(function (data) {
+        $("#thought").text(data.thing)
+        $("#range").val(data.rate)
+        $("#category").text(data.category)
+        $("#comment").text(data.comment)
+        /// DATE is automaticali created by sequelize, need to transform it and display
+        $("#date").text(data.date)
+    })
+
+    $("#delete").on("click", function () {
+        $.delete("/api/delete/:" + userId, curentEdit).then(function (data) {
+            alert("This record succsessfully deleted")
+            window.location.href = "./search";
+        })
+    })
+
+
+})
