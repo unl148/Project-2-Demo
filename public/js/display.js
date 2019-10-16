4316aab153300b64e5f0463a7f9721b3fd667586
unl148 <ksumana02@hotmail.com>
2019-10-16 13:07:10 -0700

    added displayfunctionality

---
+++ public/js/display.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  console.log("Hello from first.js");
+  $("#editButton").on("submit", function(event) {
+    event.preventDefault();
+    var thought = $("#thought")
+      .val()
+      .trim();
+    var range = $("#range").val();
+    var categoryId = $("#selectCategory").val();
+    var comment;
+
+    //Do we need this check?
+    // if ($("#comment").val()) {
+    comment = $("#comment")
+      .val()
+      .trim();
+    // } else {
+    //   comment = "No comment left";
+    // }
+
+    var userId = JSON.parse(window.localStorage.getItem("id"));
+    var newThought = {
+      title: thought,
+      rating: range,
+      CategoryId: categoryId,
+      notes: comment,
+      UserId: userId,
+      //added simple date capture, rendering ugly
+      dateTime: Date()
+    };
+    console.log(newThought);
+    // eslint-disable-next-line no-unused-vars
+    // $.put("/api/newthought/", newThought).then(function (data) {
+    //   if (data === "DB Error") {
+    //     return alert("Sorry, we have problems, try again later");
+    //   }
+    //   window.location.href = "/api/search/" + userId;
+    // });
+  });
+});
